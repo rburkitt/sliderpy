4c695d5ce87c060ba4eabfeb1524f077530afddd
Raymond Burkitt <raymondburkitt@hotmail.com>
2025-02-01 22:03:51 -0500

    refactoring button creation and seetting button text on click event

--- slider.py
+++ slider.py
@@ -5,7 +5,6 @@ import random
 import re
 
 numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", ""]
-button_dict = {} 
 
 def extract_number(string):
     match = re.search(r'\d+', string)
@@ -16,7 +15,7 @@ def click_me(sender):
     if text == "":
         return
     
-    index = numbers.index(text) #extract_number(sender.name) -1
+    index = numbers.index(text)
     cell = numbers[index]
     empty = numbers.index("")
     if empty != index - 1 and empty != index + 1 and empty != index - 4 and empty != index +4:
@@ -25,21 +24,19 @@ def click_me(sender):
     numbers[index] = ""
     numbers[empty] = cell
 
-    load_board()
+    widgets = frame.winfo_children()
+    widgets[index].config(text = numbers[index])
+    widgets[empty].config(text = numbers[empty])
 
     if ",".join(numbers) == "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,":
         messagebox.showinfo("Hurray!", "You solved it!")
 
 def load_board():
-    for widget in frame.winfo_children():
-        widget.destroy() 
-
     for i in range(4):
         for j in range(4):
-            button = tk.Button(frame, text=f'{numbers[4*i+j]}', height=5, width=5)
+            button = tk.Button(frame, text=f'{numbers[4*i+j]}', height=5, width=5, relief=tk.RAISED, borderwidth=5)
             button.configure(command=lambda b=button: click_me(b))
-            button.grid(row=i, column = j)
-            button_dict[4*i*j] = button    
+            button.grid(row=i, column = j)   
 
 def begin():
     random.shuffle(numbers)
@@ -52,7 +49,7 @@ root.title("Slider Puzzle")
 
 frame = Frame(root, bd = 1)
 frame.pack(fill = 'both', expand = True,
-           padx = 10, pady = 10)
+           padx = 15, pady = 15)
 
 begin()
 
